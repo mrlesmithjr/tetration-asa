31958897e35e6db65311133dff74da9c6d43de9f
Chris McHenry <mchenry.chris@gmail.com>
2017-06-06 08:28:31 -0400

    Updated for Tetration 2.0

---
+++ TetPolicy2.py
@@ -0,0 +1,250 @@
+from tetpyclient import RestClient
+import json
+from sets import Set
+from tqdm import tqdm
+
+class AbsolutePolicy(object):
+    def __init__(self, policy):
+        self._policy = policy
+        self._consumerFilterName = policy['consumer_filter_name']
+        self._consumerFilterID = policy['consumer_filter_id']
+        self._providerFilterName = policy['provider_filter_name']
+        self._providerFilterID = policy['provider_filter_id']
+        self._action = policy['action']
+        self._l4params = []
+        for param in l4params:
+            self._l4params.append({'port_min':param['port'][0],'port_max':param['port'][1],'proto':param['proto']})
+
+    @property
+    def action(self):
+        return self._action
+
+    @property
+    def consumerFilterName(self):
+        return self._consumerFilterName
+
+    @property
+    def providerFilterName(self):
+        return self._providerFilterName
+
+    @property
+    def consumerFilterID(self):
+        return self._consumerFilterID
+
+    @property
+    def providerFilterID(self):
+        return self._providerFilterID
+
+    @property
+    def l4params(self):
+        return self._l4params
+
+
+
+class DefaultPolicy(object):
+    def __init__(self, policy):
+        self._policy = policy
+        self._consumerFilterName = policy['consumer_filter_name']
+        self._consumerFilterID = policy['consumer_filter_id']
+        self._providerFilterName = policy['provider_filter_name']
+        self._providerFilterID = policy['provider_filter_id']
+        self._action = policy['action']
+        self._l4params = []
+        for param in policy['l4_params']:
+            self._l4params.append({'port_min':param['port'][0],'port_max':param['port'][1],'proto':param['proto']})
+
+    @property
+    def action(self):
+        return self._action
+
+    @property
+    def consumerFilterName(self):
+        return self._consumerFilterName
+
+    @property
+    def providerFilterName(self):
+        return self._providerFilterName
+
+    @property
+    def consumerFilterID(self):
+        return self._consumerFilterID
+
+    @property
+    def providerFilterID(self):
+        return self._providerFilterID
+
+    @property
+    def l4params(self):
+        return self._l4params
+
+class InventoryFilter(object):
+
+    def __init__(self, policy):
+        self._name = policy['name']
+        self._id = policy['id']
+        self._filter = policy['query']
+        self._ipSet = Set()
+        self._hosts = []
+        if 'parent_app_scope' in policy.keys():
+            self._parentScope = policy['parent_app_scope']
+        else:
+            self._parentScope = None
+
+    @property
+    def hosts(self):
+        return self._hosts
+
+    @property
+    def name(self):
+        return self._name
+
+    @property
+    def ipSet(self):
+        return self._ipSet
+
+    @property
+    def uid(self):
+        return self._id
+
+    @property
+    def parentScope(self):
+        return self._parentScope
+
+    @property
+    def ipSet(self):
+        return self._ipSet
+
+    def resolveFilter(self,environment):
+        body = json.dumps({'filter':self._filter})
+        #print body
+        resp = environment.tetClient.post('/inventory/search',json_body=body)
+        if resp:
+            ips = resp.json()
+            #print ips
+            for i in ips['results']:
+                self._ipSet.add(i['ip'])
+                self._hosts.append(i)
+        #print(self._ipSet)
+
+
+class Cluster(object):
+    def __init__(self, policy):
+        self._name = policy['name']
+        self._id = policy['id']
+        self._external = policy['external']
+        self._ipSet = Set()
+        self._hosts = []
+        for host in policy['nodes']:
+            self._hosts.append(host)
+            self._ipSet.add(host['ip'])
+
+
+    @property
+    def hosts(self):
+        return self._hosts
+
+    @property
+    def name(self):
+        return self._name
+
+    @property
+    def uid(self):
+        return self._id
+
+    @property
+    def ipSet(self):
+        return self._ipSet
+
+class App(object):
+    def __init__(self,environment,app_def):
+        self._environment = environment
+        self._absolutePolicies = []
+        self._defaultPolicies = []
+        self._inventoryFilters = {}
+        self._clusters = {}
+        self._name = app_def['name']
+        self._id = app_def['id']
+        self._appScopeId = app_def['app_scope_id']
+        self._adjacent = False
+        self.loadNestedPolicy(app_def)
+
+#    def __init__(self,environment,app_def,adjacent):
+#        self._environment = environment
+#        self._absolutePolicies = []
+#        self._defaultPolicies = []
+#        self._inventoryFilters = {}
+#        self._clusters = {}
+#        self._name = app_def['name']
+#        self._id = app_def['id']
+#        self._appScopeId = app_def['app_scope_id']
+#        self._adjacent = adjacent
+
+
+    @property
+    def absolutePolicies(self):
+        return self._absolutePolicies
+
+    @property
+    def defaultPolicies(self):
+        return self._defaultPolicies
+
+    @property
+    def inventoryFilters(self):
+        return self._inventoryFilters
+
+    @property
+    def clusters(self):
+        return self._clusters
+
+    def loadNestedPolicy(self, config):
+        print 'Resolving Inventory Filters...'
+        if 'inventory_filters' in config.keys():
+            for inventoryFilter in tqdm(config['inventory_filters']):
+                inventoryFilter = InventoryFilter(inventoryFilter)
+                inventoryFilter.resolveFilter(self._environment)
+                self._inventoryFilters[inventoryFilter.uid] = inventoryFilter
+        if 'clusters' in config.keys():
+            for cluster in config['clusters']:
+                cluster = Cluster(cluster)
+                self._clusters[cluster.uid] = cluster
+        if 'absolute_policies' in config.keys():
+            for policy in config['absolute_policies']:
+                self._absolutePolicies.append(AbsolutePolicy(policy))
+        if 'default_policies' in config.keys():
+            for policy in config['default_policies']:
+                self._defaultPolicies.append(DefaultPolicy(policy))
+
+class Environment(object):
+    def __init__(self,tetCluster,tetCreds):
+        self._primaryApps = {}
+        self._adjacentApps = {}
+        self._scopes = {}
+        self._restclient = RestClient(tetCluster,
+                        credentials_file=tetCreds,
+                        verify=False)
+
+    @property
+    def primaryApps(self):
+        return self._primaryApps
+
+    @property
+    def adjacentApps(self):
+        return self._adjacentApps
+
+    @property
+    def tetClient(self):
+        return self._restclient
+
+    @property
+    def scopes(self):
+        return self._scopes
+
+    def addScope(self, scope_id):
+        self._scopes['scope']=scope_id
+
+    def loadPolicy(self, appIDs):
+        #Load Policy JSON
+        for appID in appIDs:
+            appDetails = self._restclient.get('/openapi/v1/applications/%s/details'%appID).json()
+            print('\nProcessing "%s"...'%appDetails['name'])
+            self._primaryApps[appID] = App(environment=self,app_def=appDetails)
--- policy_asa.py
+++ policy_asa.py
@@ -1,40 +1,63 @@
 """
 Standalone application to convert Tetration Policy to CSV
 """
-from apicservice import ConfigDB
 import json
-import argparse
 import csv
-import ipcalc
+from tetpyclient import RestClient
+import requests.packages.urllib3
+from TetPolicy2 import Environment
+from terminaltables import AsciiTable
+
+def selectTetrationApps(endpoint,credentials):
+
+    restclient = RestClient(endpoint,
+                            credentials_file=credentials,
+                            verify=False)
+
+    requests.packages.urllib3.disable_warnings()
+    resp = restclient.get('/openapi/v1/applications')
+
+    if not resp:
+        sys.exit("No data returned for Tetration Apps! HTTP {}".format(resp.status_code))
+
+    app_table = []
+    app_table.append(['Number','Name','Description','Author','Primary'])
+    for i,app in enumerate(resp.json()):
+        app_table.append([i+1,app['name'],app['description'],app['author'],app['primary']])
+    print(AsciiTable(app_table).table)
+    choice = raw_input('\nSelect Tetration App: ')
+
+    choice = choice.split(',')
+    appIDs = []
+    for app in choice:
+        if '-' in app:
+            for app in range(int(app.split('-')[0])-1,int(app.split('-')[1])):
+                appIDs.append(resp.json()[int(app)-1]['id'])
+        else:
+            appIDs.append(resp.json()[int(app)-1]['id'])
+
+    return appIDs
 
 
 def main():
     """
     Main execution routine
     """
-    parser = argparse.ArgumentParser(description='Tetration Policy to CSV')
-    parser.add_argument('--maxlogfiles', type=int, default=10, help='Maximum number of log files (default is 10)')
-    parser.add_argument('--debug', nargs='?',
-                        choices=['verbose', 'warnings', 'critical'],
-                        const='critical',
-                        help='Enable debug messages.')
-    parser.add_argument('--config', default=None, help='Configuration file')
-    args = parser.parse_args()
 
-    if args.config is None:
-        print '%% No configuration file given'
-        return
+    #Tetration Access Information
+    API_ENDPOINT="https://medusa-cpoc.cisco.com"
+    API_CREDS="/Users/christophermchenry/Documents/Scripting/tetration-api/medusa_credentials.json"
+    #API_ENDPOINT="https://198.18.138.4"
+    #API_CREDS="/Users/christophermchenry/Documents/Scripting/tetration-api/vcluster_credentials.json"
 
-    # Load in the configuration
-    try:
-        with open(args.config) as config_file:
-            config = json.load(config_file)
-    except IOError:
-        print '%% Could not load configuration file'
-        return
-    except ValueError:
-        print 'Could not load improperly formatted configuration file'
-        return
+    #Select Tetration Apps and Load Tet Object Model
+    tetEnv = Environment(API_ENDPOINT,API_CREDS)
+    tetEnv.loadPolicy(appIDs=selectTetrationApps(endpoint=API_ENDPOINT,credentials=API_CREDS))
+    app = tetEnv.primaryApps[tetEnv.primaryApps.keys()[0]]
+    clusters = app.clusters
+    filters = app.inventoryFilters
+    policies = app.defaultPolicies
+
 
     # Load in the IANA Protocols
     protocols = {}
@@ -64,50 +87,43 @@ def main():
         print 'Could not load improperly formatted protocols file'
         return
 
-    cdb = ConfigDB()
-    cdb.store_config(config)
-    clusters = cdb.get_epg_policies()
-    policies = cdb.get_contract_policies()
-
-    #Process nodes and output information to CSV
-    for cluster in clusters:
-        ##Uncomment for ASA Config
+    print('\nASA ACL Config\n---------------------------------------\n\n')
+    #Process nodes and output information to ASA Objects
+    for key in clusters.keys():
+        cluster = clusters[key]
         print "object network " + cluster.name.replace(' ','_')
-        for node in cluster.get_node_policies():
-            ##Uncomment for ASA Config
-            node_dict = node.__dict__
-            #print node.__dict__['_policy']
-            if 'prefix_len' in node_dict['_policy'].keys():
-                subnet = ipcalc.Network(node.ip + '/' + str(node_dict['_policy']['prefix_len']))
-                print "  subnet " + str(subnet.network()) + ' ' + str(subnet.netmask())
-                #print node_dict['_policy']['prefix_len']
-            else:
-                print "  host " + node.ip
+        for ip in cluster.ipSet:
+            #UPDATE TO ACCOUNT FOR SUBNETS
+            print "  host " + ip
+
+    for key in filters.keys():
+        invFilter = filters[key]
+        if invFilter.name != 'Default':
+            print "object network " + invFilter.name.replace(' ','_')
+            for ip in invFilter.ipSet:
+            #UPDATE TO ACCOUNT FOR SUBNETS
+                print "  host " + ip
 
     print '!'
 
-    #Process policies and output information to CSV
+    #Process policies and output information as ASA ACL Lines
     for policy in policies:
-        for rule in policy.get_whitelist_policies():
-
-            ##Uncomment for ASA Config
-            #if policy.src_name == 'External' and policy.dst_name != 'External':
-            if policy.src_name != policy.dst_name:
-                if rule.proto == '1':
-                    #print "access-list ACL_IN extended permit " + protocols[rule.proto]['Keyword'] + " object-group " + policy.src_name.replace(' ','_') + " object-group " + policy.dst_name.replace(' ','_')
-                    print "access-list ACL_IN extended permit " + protocols[rule.proto]['Keyword'] + ((" object " + policy.src_name.replace(' ','_')) if policy.src_name != 'External' else " any") + ((" object " + policy.dst_name.replace(' ','_')) if policy.dst_name != 'External' else " any")
-                elif (rule.proto == '6') or (rule.proto == '17'):
-                    if rule.port_min == rule.port_max:
-                        if (str(rule.port_min) in ports.keys()) and (ports[str(rule.port_min)]['Proto'] == protocols[rule.proto]['Keyword'] or ports[str(rule.port_min)]['Proto'] == 'TCP, UDP'):
-                            port = ports[str(rule.port_min)]['Name']
+        for rule in policy.l4params:
+            #if policy.consumerFilterName == 'Default' and policy.providerFilterName != 'Default':
+            if policy.consumerFilterName != policy.providerFilterName:
+                if rule['proto'] == 1:
+                    print "access-list ACL_IN extended permit " + protocols[str(rule['proto'])]['Keyword'] + ((" object " + policy.consumerFilterName.replace(' ','_')) if policy.providerFilterName != 'Default' else " any") + ((" object " + policy.providerFilterName.replace(' ','_')) if policy.providerFilterName != 'Default' else " any")
+                elif (rule['proto'] == 6) or (rule['proto'] == 17):
+                    if rule['port_min'] == rule['port_max']:
+                        if (str(rule['port_min']) in ports.keys()) and (ports[str(rule['port_min'])]['Proto'] == protocols[str(rule['proto'])]['Keyword'] or ports[str(rule['port_min'])]['Proto'] == 'TCP, UDP'):
+                            port = ports[str(rule['port_min'])]['Name']
                         else:
-                            port = rule.port_min
-                        #print "access-list ACL_IN extended permit " + protocols[rule.proto]['Keyword'] + " object-group " + policy.src_name.replace(' ','_') + " object-group " + policy.dst_name.replace(' ','_') + " eq " + rule.port_min
-                        print "access-list ACL_IN extended permit " + protocols[rule.proto]['Keyword'] + ((" object " + policy.src_name.replace(' ','_')) if policy.src_name != 'External' else " any") + ((" object " + policy.dst_name.replace(' ','_')) if policy.dst_name != 'External' else " any") + " eq " + port
+                            port = rule['port_min']
+                        print "access-list ACL_IN extended permit " + protocols[str(rule['proto'])]['Keyword'] + ((" object " + policy.consumerFilterName.replace(' ','_')) if policy.consumerFilterName != 'Default' else " any") + ((" object " + policy.providerFilterName.replace(' ','_')) if policy.providerFilterName != 'Default' else " any") + " eq " + str(port)
                     else:
-                        #print "access-list ACL_IN extended permit " + protocols[rule.proto]['Keyword'] + " object-group " + policy.src_name.replace(' ','_') + " object-group " + policy.dst_name.replace(' ','_') + " range " + rule.port_min + "-" + rule.port_max
-                        print "access-list ACL_IN extended permit " + protocols[rule.proto]['Keyword'] + ((" object " + policy.src_name.replace(' ','_')) if policy.src_name != 'External' else " any") + ((" object " + policy.dst_name.replace(' ','_')) if policy.dst_name != 'External' else " any") + " range " + rule.port_min + "-" + rule.port_max
+                        print "access-list ACL_IN extended permit " + protocols[str(rule['proto'])]['Keyword'] + ((" object " + policy.consumerFilterName.replace(' ','_')) if policy.consumerFilterName != 'Default' else " any") + ((" object " + policy.providerFilterName.replace(' ','_')) if policy.providerFilterName != 'Default' else " any") + " range " + str(rule['port_min']) + "-" + str(rule['port_max'])
 
+    print "access-list ACL_IN extended deny ip any any\n!\n\n"
 
 
 if __name__ == '__main__':
